8f8687ff81892874a32c6a556c6be2e686012731
Kamal Kant <kamalkant.k4@gmail.com>
2021-06-29 20:04:42 -0700

    included ppo & dueling dqn for intersection environment

---
+++ single-agent/env-intersection/dueling-dqn/dueling-dqn-intersection.py
@@ -0,0 +1,223 @@
+import math
+import random
+import gym
+import highway_env
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.autograd as autograd
+import torch.nn.functional as F
+from IPython.display import clear_output
+import matplotlib.pyplot as plt
+from collections import deque
+
+
+class ReplayBuffer(object):
+    def __init__(self, capacity):
+        self.buffer = deque(maxlen=capacity)
+
+    def push(self, state, action, reward, next_state, done):
+        state = np.expand_dims(state, 0)
+        next_state = np.expand_dims(next_state, 0)
+
+        self.buffer.append((state, action, reward, next_state, done))
+
+    def sample(self, batch_size):
+        state, action, reward, next_state, done = zip(
+            *random.sample(self.buffer, batch_size))
+        return np.concatenate(state, axis=0), action, reward, np.concatenate(next_state), done
+
+    def __len__(self):
+        return len(self.buffer)
+
+
+class Model(nn.Module):
+    def __init__(self, num_inputs, num_actions):
+        super(Model, self).__init__()
+
+        self.num_inputs = num_inputs
+        self.num_actions = num_actions
+
+        self.feature = nn.Sequential(
+            nn.Linear(self.num_inputs, 128),
+            nn.ReLU()
+        )
+
+        self.advantage = nn.Sequential(
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, self.num_actions)
+        )
+
+        self.value = nn.Sequential(
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1)
+        )
+
+    def forward(self, x):
+        x = torch.flatten(x, start_dim=1)
+        f = self.feature(x)
+        advantage = self.advantage(f)
+        value = self.value(f)
+        return value + advantage - advantage.mean()
+
+    def act(self, state, epsilon):
+        if random.random() > epsilon:
+            state = torch.FloatTensor(state).unsqueeze(0)
+            q_value = self.forward(state)
+            action = q_value.argmax()
+            action = action.item()
+        else:
+            action = random.randrange(self.num_actions)
+        return action
+
+
+class Dueling_DQN:
+    def __init__(self):
+        pass
+
+    def init_hyp(self):
+        self.epsilon_start = 1.0
+        self.epsilon_final = 0.01
+        self.epsilon_decay = 500
+        self.epsilon_by_frame = lambda frame_idx: self.epsilon_final + \
+            (self.epsilon_start - self.epsilon_final) * \
+            math.exp(-1. * frame_idx / self.epsilon_decay)
+        self.num_frames = 10000
+        self.batch_size = 32
+        self.gamma = 0.99
+        self.losses = []
+        self.all_rewards = []
+        self.episode_reward = 0
+        self.checkpoint_path = "single-agent/env-intersection/dueling-dqn/checkpoints/dueling-dqn-intersection.pth"
+        self.do_render = True
+        self.do_show_log = True
+
+    def init_env(self):
+        env_id = "intersection-v0"
+        self.env = gym.make(env_id)
+
+        print("obs space = {} action space = {} ".format(self.env.observation_space.shape,
+              self.env.action_space.n))
+
+    def create_model(self):
+        self.current_model = Model(
+            np.prod(self.env.observation_space.shape), self.env.action_space.n)
+        self.target_model = Model(
+            np.prod(self.env.observation_space.shape), self.env.action_space.n)
+
+        self.optimizer = optim.Adam(self.current_model.parameters())
+        self.replay_buffer = ReplayBuffer(1000)
+        self.update_target(self.current_model, self.target_model)
+        self.save_model()
+
+    def update_target(self, current_model, target_model):
+        self.target_model.load_state_dict(self.current_model.state_dict())
+
+    def compute_td_loss(self, batch_size):
+        state, action, reward, next_state, done = self.replay_buffer.sample(
+            batch_size)
+
+        state = torch.FloatTensor(np.float32(state))
+        next_state = torch.FloatTensor(np.float32(next_state))
+        action = torch.LongTensor(action)
+        reward = torch.FloatTensor(reward)
+        done = torch.FloatTensor(done)
+
+        q_values = self.current_model(state)
+        next_q_values = self.target_model(next_state)
+
+        q_value = q_values.gather(1, action.unsqueeze(1)).squeeze(1)
+        next_q_value = next_q_values.max(1)[0]
+        expected_q_value = reward + self.gamma * next_q_value * (1 - done)
+
+        loss = (q_value - expected_q_value.data).pow(2).mean()
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        return loss
+
+    def plot(self, frame_idx, rewards, losses):
+        clear_output(True)
+        plt.figure(figsize=(20, 5))
+        plt.subplot(131)
+        plt.title('frame %s. reward: %s' % (frame_idx, np.mean(rewards[-10:])))
+        plt.plot(rewards)
+        plt.subplot(132)
+        plt.title('loss')
+        plt.plot(losses)
+        plt.show()
+
+    def plot_epsilon():
+        plt.plot([self.epsilon_by_frame(i) for i in range(10000)])
+
+    def train(self):
+        state = self.env.reset()
+        for frame_idx in range(1, self.num_frames + 1):
+            epsilon = self.epsilon_by_frame(frame_idx)
+            action = self.current_model.act(state, epsilon)
+
+            next_state, reward, done, _ = self.env.step(action)
+            if self.do_render:
+                self.env.render()
+            self.replay_buffer.push(state, action, reward, next_state, done)
+
+            state = next_state
+            self.episode_reward += reward
+
+            if done:
+                state = self.env.reset()
+                self.all_rewards.append(self.episode_reward)
+                self.episode_reward = 0
+
+            if len(self.replay_buffer) > self.batch_size:
+                loss = self.compute_td_loss(self.batch_size)
+                self.losses.append(loss.item())
+
+            if frame_idx % 200 == 0 and self.do_show_log:
+                self.plot(frame_idx, self.all_rewards, self.losses)
+                print("average reward = {}".format(np.mean(self.all_rewards[-10:])))
+
+            if frame_idx % 100 == 0:
+                self.update_target(self.current_model, self.target_model)
+                self.save_model()
+
+    def save_model(self):
+        torch.save(self.target_model, self.checkpoint_path)
+
+
+# standalone function evaluate model on the environment
+def evaluate():
+    checkpoint_path = "single-agent/env-intersection/dueling-dqn/checkpoints/dueling-dqn-intersection.pth"
+    model = torch.load(checkpoint_path)
+    env = gym.make("intersection-v0")
+    state = env.reset()
+    done = False
+
+    while done != True:
+        state = torch.FloatTensor(state).unsqueeze(0)
+        q_value = model(state)
+        action = q_value.argmax()
+        action = action.item()
+        state, reward, done, info = env.step(action)
+        env.render()
+
+
+def main():
+    obj = Dueling_DQN()
+    obj.init_hyp()
+    obj.init_env()
+    obj.create_model()
+
+    obj.train()
+
+    # run when the model is trained
+    # evaluate()
+  
+
+if __name__=="__main__":
+    main()
---
+++ single-agent/env-intersection/ppo/ppo-highway.py
@@ -0,0 +1,210 @@
+import argparse
+import pickle
+from collections import namedtuple
+from itertools import count
+import os
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import gym
+import highway_env
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Normal, Categorical
+from torch.utils.data.sampler import BatchSampler, SubsetRandomSampler
+import matplotlib.pyplot as plt
+
+
+class Actor(nn.Module):
+    def __init__(self, num_inputs, num_actions):
+        super(Actor, self).__init__()
+        self.fc1 = nn.Linear(num_inputs, 100)
+        self.action_head = nn.Linear(100, num_actions)
+
+    def forward(self, x):
+        x = torch.flatten(x, start_dim=1)
+        x = F.relu(self.fc1(x))
+        action_prob = F.softmax(self.action_head(x), dim=1)
+        return action_prob
+
+
+class Critic(nn.Module):
+    def __init__(self, num_inputs, num_actions):
+        super(Critic, self).__init__()
+        self.fc1 = nn.Linear(num_inputs, 100)
+        self.state_value = nn.Linear(100, 1)
+
+    def forward(self, x):
+        x = torch.flatten(x, start_dim=1)
+        x = F.relu(self.fc1(x))
+        value = self.state_value(x)
+        return value
+
+
+class PPO():
+    def __init__(self):
+        super(PPO, self).__init__()
+
+    def init_hyp(self):
+        self.buffer = []
+        self.counter = 0
+        self.training_step = 0
+        self.clip_param = 0.2
+        self.max_grad_norm = 0.5
+        self.ppo_update_time = 10
+        self.buffer_capacity = 1000
+        self.batch_size = 32
+        self.gamma = 0.99
+        self.render = True
+        self.seed = 1
+        self.log_interval = 10
+        self.checkpoint_path = "single-agent/env-intersection/ppo/checkpoints/ppo-intersection.pth"
+
+    def init_env(self):
+        self.env = gym.make('intersection-v0')
+        self.num_state = self.env.observation_space.shape[0]
+        self.num_action = self.env.action_space.n
+        torch.manual_seed(self.seed)
+        self.env.seed(self.seed)
+
+        print("obs space = {} action space = {} ".format(self.env.observation_space.shape,
+              self.env.action_space.n))
+
+    def create_model(self):
+        self.actor_net = Actor(
+            np.prod(self.env.observation_space.shape), self.env.action_space.n)
+        self.critic_net = Critic(
+            np.prod(self.env.observation_space.shape), self.env.action_space.n)
+        self.actor_optimizer = optim.Adam(self.actor_net.parameters(), 1e-3)
+        self.critic_net_optimizer = optim.Adam(
+            self.critic_net.parameters(), 3e-3)
+
+    def select_action(self, state):
+        state = torch.from_numpy(state).float().unsqueeze(0)
+        with torch.no_grad():
+            action_prob = self.actor_net(state)
+        c = Categorical(action_prob)
+        action = c.sample()
+        return action.item(), action_prob[:, action.item()].item()
+
+    def get_value(self, state):
+        state = torch.from_numpy(state)
+        with torch.no_grad():
+            value = self.critic_net(state)
+        return value.item()
+
+    def store_transition(self, transition):
+        self.buffer.append(transition)
+        self.counter += 1
+
+    def update(self, i_ep):
+        state = torch.tensor([t.state for t in self.buffer], dtype=torch.float)
+        action = torch.tensor(
+            [t.action for t in self.buffer], dtype=torch.long).view(-1, 1)
+        reward = [t.reward for t in self.buffer]
+        old_action_log_prob = torch.tensor(
+            [t.a_log_prob for t in self.buffer], dtype=torch.float).view(-1, 1)
+
+        R = 0
+        Gt = []
+        for r in reward[::-1]:
+            R = r + self.gamma * R
+            Gt.insert(0, R)
+        Gt = torch.tensor(Gt, dtype=torch.float)
+
+        for i in range(self.ppo_update_time):
+            for index in BatchSampler(SubsetRandomSampler(range(len(self.buffer))), self.batch_size, False):
+                if self.training_step % 1000 == 0:
+                    print('I_ep {} ，train {} times'.format(
+                        i_ep, self.training_step))
+
+                Gt_index = Gt[index].view(-1, 1)
+                V = self.critic_net(state[index])
+                delta = Gt_index - V
+                advantage = delta.detach()
+
+                action_prob = self.actor_net(state[index]).gather(
+                    1, action[index])
+
+                ratio = (action_prob/old_action_log_prob[index])
+                surr1 = ratio * advantage
+                surr2 = torch.clamp(ratio, 1 - self.clip_param,
+                                    1 + self.clip_param) * advantage
+
+                action_loss = -torch.min(surr1, surr2).mean()
+
+                print("loss= {}, step = {}".format(
+                    action_loss.item(), self.training_step))
+
+                self.actor_optimizer.zero_grad()
+                action_loss.backward()
+                nn.utils.clip_grad_norm_(
+                    self.actor_net.parameters(), self.max_grad_norm)
+                self.actor_optimizer.step()
+
+                value_loss = F.mse_loss(Gt_index, V)
+                self.critic_net_optimizer.zero_grad()
+                value_loss.backward()
+                nn.utils.clip_grad_norm_(
+                    self.critic_net.parameters(), self.max_grad_norm)
+                self.critic_net_optimizer.step()
+                self.training_step += 1
+
+        del self.buffer[:]
+
+    def save_model(self):
+        torch.save(self.critic_net, self.checkpoint_path)
+
+
+def main():
+    Transition = namedtuple(
+        'Transition', ['state', 'action',  'a_log_prob', 'reward', 'next_state'])
+
+    agent = PPO()
+    agent.init_hyp()
+    agent.init_env()
+    agent.create_model()
+    for i_epoch in range(1000):
+        state = agent.env.reset()
+        if agent.render:
+            agent.env.render()
+
+        for t in count():
+            action, action_prob = agent.select_action(state)
+            next_state, reward, done, _ = agent.env.step(action)
+            trans = Transition(state, action, action_prob, reward, next_state)
+            if agent.render:
+                agent.env.render()
+            agent.store_transition(trans)
+            state = next_state
+
+            if done:
+                if len(agent.buffer) >= agent.batch_size:
+                    agent.update(i_epoch)
+                    agent.save_model()
+                break
+
+
+# standalone function evaluate model on the environment
+def evaluate():
+    checkpoint_path = "single-agent/env-intersection/ppo/checkpoints/ppo-intersection.pth"
+    model = torch.load(checkpoint_path)
+    env = gym.make("intersection-v0")
+    state = env.reset()
+    done = False
+
+    while done != True:
+        state = torch.from_numpy(state).float().unsqueeze(0)
+        with torch.no_grad():
+            action_prob = model(state)
+        c = Categorical(action_prob)
+        action = c.sample()
+        state, reward, done, info = env.step(action.item())
+        env.render()
+
+
+if __name__ == '__main__':
+    main()
+    # evaluate()
